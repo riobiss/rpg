40f7a5329a187e71f0f060bc6e41937f29ffe53a
Riobis <riobisofc@gmail.com>
2025-12-30 15:27:04 -0300

    add entities 'enemy' 'player'

---
+++ src/entities/enemy.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "Goblin",
+    damage: 12,
+    health: 40,
+    defense: 4,
+  },
+  {
+    name: "Ogro",
+    damage: 18,
+    health: 100,
+    defense: 10,
+  },
+]
---
+++ src/entities/player.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: "Cristo",
+    health: 70,
+    damage: 12,
+    defense: 10,
+  },
+  {
+    name: "Jonas",
+    health: 50,
+    damage: 18,
+    defense: 3,
+  },
+  {
+    name: "Jales",
+    health: 60,
+    damage: 14,
+    defense: 7,
+  },
+]
